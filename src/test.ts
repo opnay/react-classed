fc85f22a4f63f279b61898909936788e5119bb55
Kim In Seop <opnayn@gmail.com>
2020-04-05 01:24:49 +0900

    Cleanup
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import { ClassedRef } from './Classed';
-
-const Test = ClassedRef('Test', 'this-is-test', 'div');
-
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(Test).toBeTruthy();
-  });
-});
